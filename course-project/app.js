b2a7f9b1bef54288e12668b45fec06422c83001d
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2020-07-29 17:58:26 -0400

    [ADD]: model view controller

--- course-project/app.js
+++ course-project/app.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error')
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -15,7 +16,7 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
