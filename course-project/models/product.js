5cad9cd24585dd08a1772314a1a1bade4ac9ede0
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2020-07-29 21:37:04 -0400

    [UPD]: Enchancing the app

--- course-project/models/product.js
+++ course-project/models/product.js
@@ -18,15 +18,21 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+
   }
 
   save() {
     getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if(err) {
+          console.log(err)
+        }
       });
     });
   }
