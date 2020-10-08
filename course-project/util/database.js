5504518d4a678ebcd97e2616239ebd1aaee2fa45
alfcastillo90 <cacr1990@gmail.com>
2020-10-07 22:18:27 -0300

    [ADD]: conexion con base de datos en mongodb y registro de productos

--- course-project/util/database.js
+++ course-project/util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize').Sequelize;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const uri =  "mongodb+srv://alfre:nsG8o0n5fjll2Dtl@cluster0.z0tzn.mongodb.net/shop?retryWrites=true&w=majority";
+let _db;
 
-const sequelize = new Sequelize('node-complete', 'root', '1234', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(uri)
+  .then(client => {
+    console.log('Connected');
+    _db = client.db()
+    callback();
+  })
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
+};
 
-module.exports = sequelize;
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
